96365c37cfa7a2eaf7553f8fe588c9eae4ae0809
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-01-26 20:40:52 -0300

    feat(currency): add toCurrency function for formatting currency values

---
+++ src/lib/toCurrency.ts
@@ -0,0 +1,6 @@
+export function toCurrency(value: number, locale = 'pt-BR', currency = 'BRL'): string {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+  }).format(value)
+}
