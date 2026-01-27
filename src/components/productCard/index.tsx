a396b61f82aaeeb62cad431065484b783353a5c3
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-01-26 21:03:01 -0300

    refactor(ProductCard): move to a dedicated folder

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,6 +1,7 @@
-import { cn } from "../lib/tailwind"
-import { toCurrency } from "../lib/toCurrency"
-import { Rating } from "./Rating"
+import { cn } from "../../lib/tailwind"
+import { toCurrency } from "../../lib/toCurrency"
+import { Rating } from "../Rating"
+import Shopping from '@/assets/shopping.svg?react'
 
 interface ImageProps {
   src: string
@@ -31,6 +32,7 @@ export function ProductCard (props: ProductCardProps) {
       'bg-neutral-100 p-3 w-full max-w-86.5 rounded-xl',
       'flex gap-2 items-start',
       'md:flex-col md:max-w-[168px]',
+      'overflow-hidden',
     )}>
       <div className="min-w-36 min-h-36 bg-surface-low-contrast relative overflow-hidden rounded-sm">
         {badge && (
@@ -43,9 +45,12 @@ export function ProductCard (props: ProductCardProps) {
             alt={image.alt} 
           />
         )}
+        <div className="absolute bottom-0 right-0 size-[36px] flex items-center justify-center bg-primary rounded-sm">
+          <Shopping />
+        </div>
       </div>
-      <div className="w-full flex flex-col gap-4">
-        <strong className="body-small--bold text-surface-contrast">{title}</strong>
+      <div className="w-full flex flex-col gap-4 overflow-hidden">
+        <strong className="body-small--bold text-surface-contrast wrap-break-word">{title}</strong>
         <div className="flex flex-col gap-2">
           <div className="flex flex-col gap-1">
             {originalPrice && (
