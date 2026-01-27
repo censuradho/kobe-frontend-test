30198effb2f85f3f1acddcef63468ecbcb37c743
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-01-27 14:27:28 -0300

    feat(TextField): add TextField component for input with label and send button

---
+++ src/components/textField/index.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/tailwind";
+import type { InputHTMLAttributes } from "react";
+import Send from '@/assets/send.svg?react'
+
+interface TextFieldProps extends 
+  Pick<InputHTMLAttributes<HTMLInputElement>, 
+  'value' | 
+  'onChange' | 
+  'placeholder' | 
+  'type' | 
+  'id' 
+  > {
+    label: string
+  }
+
+export function TextField (props: TextFieldProps) {
+  const {
+    id,
+    label,
+    ...otherProps
+  } = props
+
+  return (
+    <div className="relative flex items-center">
+      <input
+        className={cn(
+          'w-full p-2 border-surface-contrast border rounded-xl px-4',
+        )}
+        aria-label={label}
+        id={id}
+        {...otherProps}
+      />
+      <button 
+        className="absolute right-4 cursor-pointer" aria-label="Enviar"
+      >
+        <Send  />
+      </button>
+    </div>
+  )
+}
