b0d72355ecfcb09d0416eb9a56a936794118fba1
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-01-27 19:01:45 -0300

    refactor(App): enhance layout with overflow handling and background styling

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,25 @@ function App() {
   const chat = useChat()
 
   return (
-    <div className="w-full h-dvh flex flex-col justify-center items-center">
+    <div className="w-full h-dvh overflow-auto flex flex-col justify-center items-center md:px-4">
       <main className={cn(
-        'app px-4 pb-4 flex flex-col',
-        'md:h-175 md:w-full md:max-w-268.5 md:rounded-[14px]'
+        'app px-4 pb-4 flex flex-col bg-background',
+        'h-full md:h-175 w-full md:max-w-268.5 md:rounded-[14px]'
       )}>
-        <header className="w-full px-4 py-2.5 flex items-center justify-end">
+        <header className="w-full px-4 py-2.5 flex items-center justify-end sticky bg-background top-0 md:pr-6">
           <button className="cursor-pointer">
             <Close />
           </button>
         </header>
-        <div className="flex flex-1 flex-col gap-4 py-6 pr-4 overflow-y-auto">
+        <div className="flex flex-1 flex-col gap-4 py-6 md:pr-4 overflow-y-auto">
           <ChatFactory {...chat} />
         </div>
-        <form onSubmit={event => {
-          event.preventDefault()
-          chat.onUserNext()
-        }}>
+        <form 
+          className="sticky bottom-0 pt-2"
+          onSubmit={event => {
+            event.preventDefault()
+            chat.onUserNext()
+          }}>
           <TextField 
             disabled={chat.next?.type !== 'user'}
             label="Pergunte"
