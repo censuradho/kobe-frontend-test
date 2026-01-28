09fefb845bd53409f624b5aa48008511bb6222cc
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-01-28 08:26:57 -0300

    feat(UserBubble): add aria-label to paragraph for improved accessibility

--- src/components/userBubble/index.tsx
+++ src/components/userBubble/index.tsx
@@ -7,6 +7,9 @@ export function UserBubble (props: UserBubbleProps) {
   const { message } = props;
 
   return (
-    <p className="bg-primary max-w-[500px] rounded-lg rounded-tr-[2px] p-2 text-surface ml-auto">{message}</p>
+    <p 
+      aria-label={message}
+      className="bg-primary max-w-[500px] rounded-lg rounded-tr-[2px] p-2 text-surface ml-auto"
+    >{message}</p>
   )
 }
