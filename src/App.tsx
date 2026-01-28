fdff593c6bfe79784fa62f88a209718b8322f701
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-01-28 14:12:34 -0300

    refactor(App): adjust max-width for improved layout consistency

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <div className="w-full h-dvh overflow-auto flex flex-col justify-center items-center md:px-4">
       <main className={cn(
         'app px-4 pb-4 flex flex-col bg-background',
-        'h-full md:h-175 w-full md:max-w-268.5 md:rounded-[14px]'
+        'h-full md:h-175 w-full md:max-w-203 lg:max-w-268.5 md:rounded-[14px]'
       )}>
         <header className="w-full px-4 py-2.5 flex items-center justify-end sticky bg-background top-0 md:pr-6">
           <button className="cursor-pointer">
